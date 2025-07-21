03789eb5a075785c8cf500bc7cd7e3a74b26450f
wclimaco2025 <william.climaco@outlook.com>
2025-07-21 17:38:43 -0600

    Version inicial de la app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {rules:{
+     "semi":['error',"always"],
+     "quote":['error',"always"]
+  }
+}
 ];
 
 export default eslintConfig;
